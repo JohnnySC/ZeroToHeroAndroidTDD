e25f9685d417ef06a4b5f0336945f39ecf3e198f
hovhannes <johnnysc091@gmail.com>
2023-10-18 19:10:22 +0900

    Task002Test add id

--- app/src/androidTest/java/ru/easycode/zerotoheroandroidtdd/Task002Test.kt
+++ app/src/androidTest/java/ru/easycode/zerotoheroandroidtdd/Task002Test.kt
@@ -2,7 +2,7 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -12,13 +12,13 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class Task001Test {
+class Task002Test {
 
     @get:Rule
     var activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
 
     @Test
-    fun test_change_text() {
-        onView(withText("I am an Android Developer!")).check(matches(isDisplayed()))
+    fun test_add_id() {
+        onView(withId(R.id.titleTextView)).check(matches(withText("I am an Android Developer!")))
     }
 }
