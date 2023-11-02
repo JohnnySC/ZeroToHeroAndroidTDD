dcd398bfa7ed69c543a0390c4102980324ba8bbf
hovhannes <johnnysc091@gmail.com>
2023-11-02 21:47:18 +0900

    Task017Test mvvm process recreate

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -42,6 +45,7 @@ dependencies {
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
---
+++ app/src/test/java/ru/easycode/zerotoheroandroidtdd/MainViewModelTest.kt
@@ -0,0 +1,122 @@
+package ru.easycode.zerotoheroandroidtdd
+
+import androidx.lifecycle.LiveData
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+
+/**
+ * Please also check out the ui test
+ * @see ru.easycode.zerotoheroandroidtdd.Task017Test
+ */
+class MainViewModelTest {
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Before
+    fun setup() {
+        Dispatchers.setMain(UnconfinedTestDispatcher())
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
+
+    private lateinit var repository: FakeRepository
+    private lateinit var liveDataWrapper: FakeLiveDataWrapper
+    private lateinit var viewModel: MainViewModel
+
+    @Before
+    fun initialize() {
+        repository = FakeRepository.Base()
+        liveDataWrapper = FakeLiveDataWrapper.Base()
+        viewModel = MainViewModel(
+            liveDataWrapper = liveDataWrapper,
+            repository = repository
+        )
+    }
+
+    @Test
+    fun test() {
+        viewModel.load()
+        liveDataWrapper.checkUpdateCalls(listOf(UiState.ShowProgress, UiState.ShowData))
+        repository.checkLoadCalledTimes(1)
+
+        val bundleWrapper: BundleWrapper.Mutable = FakeBundleWrapper.Base()
+        val bundleWrapperSave: BundleWrapper.Save = bundleWrapper
+        val bundleWrapperRestore: BundleWrapper.Restore = bundleWrapper
+
+        viewModel.save(bundleWrapper = bundleWrapperSave)
+
+        initialize()
+
+        viewModel.restore(bundleWrapper = bundleWrapperRestore)
+        liveDataWrapper.checkUpdateCalls(listOf(UiState.ShowData))
+        repository.checkLoadCalledTimes(0)
+    }
+}
+
+private interface FakeBundleWrapper : BundleWrapper.Mutable {
+
+    class Base : FakeBundleWrapper {
+
+        private var uiState: UiState? = null
+
+        override fun save(uiState: UiState) {
+            this.uiState = uiState
+        }
+
+        override fun restore(): UiState = uiState!!
+    }
+}
+
+private interface FakeLiveDataWrapper : LiveDataWrapper {
+
+    fun checkUpdateCalls(expected: List<UiState>)
+
+    class Base : FakeLiveDataWrapper {
+
+        private val actualCallsList = mutableListOf<UiState>()
+
+        override fun checkUpdateCalls(expected: List<UiState>) {
+            assertEquals(expected, actualCallsList)
+        }
+
+        override fun save(bundleWrapper: BundleWrapper.Save) {
+            bundleWrapper.save(actualCallsList.last())
+        }
+
+        override fun update(value: UiState) {
+            actualCallsList.add(value)
+        }
+
+        override fun liveData(): LiveData<UiState> {
+            throw IllegalStateException("not used in test")
+        }
+    }
+}
+
+private interface FakeRepository : Repository {
+
+    fun checkLoadCalledTimes(times: Int)
+
+    class Base : FakeRepository {
+
+        private var actualCalledTimes: Int = 0
+
+        override fun checkLoadCalledTimes(times: Int) {
+            assertEquals(times, actualCalledTimes)
+        }
+
+        override suspend fun load() {
+            actualCalledTimes++
+        }
+    }
+}
