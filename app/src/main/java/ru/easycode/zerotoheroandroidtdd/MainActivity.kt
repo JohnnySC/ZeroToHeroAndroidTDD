50608410475c398ac01f8997aeace8cda940172f
Ivan Kliachenko <ivan.green69@gmail.com>
2023-11-22 16:27:09 +0200

    task/009 solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,44 @@
 package ru.easycode.zerotoheroandroidtdd
 
+import android.annotation.SuppressLint
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var textView: TextView
+    private lateinit var linearLayout: LinearLayout
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textView = findViewById(R.id.titleTextView)
+        val button = findViewById<Button>(R.id.removeButton)
+        linearLayout = findViewById(R.id.rootLayout)
+
+        savedInstanceState.let {
+            val removeTextView = savedInstanceState?.getBoolean(KEY)
+            if (removeTextView == true) linearLayout.removeView(textView)
+        }
+
+        button.setOnClickListener {
+            linearLayout.removeView(textView)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val removeTextView = linearLayout.childCount == 1
+        outState.putBoolean(KEY, removeTextView)
+    }
+
+    companion object {
+        private const val KEY = "key"
     }
 }
