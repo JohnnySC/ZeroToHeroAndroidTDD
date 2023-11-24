7f25b1bcd0a954c477279777b076e809e9a801f1
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-11-24 21:42:21 +0900

    task/006 - solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,23 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val textView = binding.titleTextView
+        val button = binding.changeButton
+        button.setOnClickListener { changeTextTextView(textView) }
+
     }
+
+    private fun changeTextTextView(textView: TextView) {
+        textView.setText(R.string.new_text_tv)
+    }
+
 }
