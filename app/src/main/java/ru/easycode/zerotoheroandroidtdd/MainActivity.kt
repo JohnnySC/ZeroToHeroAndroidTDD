d72cf28c684cfc7128cc87fb89cf74d09570f0ac
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-11-27 22:34:03 +0900

    task/007 - solved save bundle

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -6,14 +6,30 @@ import android.widget.TextView
 import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var textView: TextView
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val textView = binding.titleTextView
+        textView = binding.titleTextView
         val button = binding.changeButton
         button.setOnClickListener { changeTextTextView(textView) }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("key", textView.text.toString())
+    }
+
+    override fun onRestoreInstanceState(
+        savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        textView.text = savedInstanceState.getString("key")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
     }
 
     private fun changeTextTextView(textView: TextView) {
