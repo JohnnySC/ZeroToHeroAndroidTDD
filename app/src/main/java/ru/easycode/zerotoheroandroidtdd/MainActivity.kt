9169ad5b8214fb0c420ddccd3f628bf466f3a0f0
Artur Rahimzyanov <vlvvvvl@yandex.ru>
2023-12-20 19:28:15 +0300

    task 9 done

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,45 @@
 package ru.easycode.zerotoheroandroidtdd
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var text: TextView
+    lateinit var button: Button
+    lateinit var a: LinearLayout
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button = findViewById(R.id.removeButton)
+        text = findViewById(R.id.titleTextView)
+        a = findViewById(R.id.rootLayout);
+
+        button.setOnClickListener {
+            a.removeView(text)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean("key1",  true)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        if(savedInstanceState.getBoolean("key1")) {
+            a.removeView(text)
+        }
     }
 }
