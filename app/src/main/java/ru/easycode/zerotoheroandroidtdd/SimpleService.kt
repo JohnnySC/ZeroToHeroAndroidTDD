798df13ed3e3b9e00186d07d02b31e07a04fe07e
Ivan Kliachenko <ivan.green69@gmail.com>
2023-12-27 22:54:25 +0200

    delete comment in SimpleService + merge task/019

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/SimpleService.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/SimpleService.kt
@@ -3,7 +3,6 @@ package ru.easycode.zerotoheroandroidtdd
 import com.google.gson.annotations.SerializedName
 import retrofit2.http.GET
 import retrofit2.http.Path
-import retrofit2.http.Url
 
 interface SimpleService {
 
