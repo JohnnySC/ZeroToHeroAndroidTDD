b633c8581243c1a47d99be059d93f72e929647a1
hovhannes <johnnysc091@gmail.com>
2024-03-20 16:34:18 +0300

    task-030 compose hello world

--- app/src/androidTest/java/ru/easycode/zerotoheroandroidtdd/Task030Test.kt
+++ app/src/androidTest/java/ru/easycode/zerotoheroandroidtdd/Task030Test.kt
@@ -1,6 +1,7 @@
 package ru.easycode.zerotoheroandroidtdd
 
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Assert.*
 import org.junit.Rule
@@ -8,13 +9,13 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class InitialUiTest {
+class Task030Test {
 
     @get:Rule
     val composeTestRule = createAndroidComposeRule<MainActivity>()
 
     @Test
-    fun test() {
-        //todo
+    fun test_hello_world() {
+        composeTestRule.onNodeWithText("Hello World!").assertExists()
     }
 }
