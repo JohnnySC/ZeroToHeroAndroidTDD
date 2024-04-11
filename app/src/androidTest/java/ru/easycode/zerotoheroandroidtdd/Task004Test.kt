bd4c2eb98cf384614a466796811fe21a6d009067
ArsenKoch <arsenkocaran@icloud.com>
2024-04-11 21:22:31 +0300

    Add id,text, button and LinearLayout in xml

--- app/src/androidTest/java/ru/easycode/zerotoheroandroidtdd/Task004Test.kt
+++ app/src/androidTest/java/ru/easycode/zerotoheroandroidtdd/Task004Test.kt
@@ -13,7 +13,6 @@ import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.hamcrest.Matchers.allOf
-import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
