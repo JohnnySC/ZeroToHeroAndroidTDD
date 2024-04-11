36878f8cca08aa54794d64f403a8fb5c1ac311bc
ArsenKoch <arsenkocaran@icloud.com>
2024-04-11 20:43:27 +0300

    Add id and change text in xml

--- app/src/androidTest/java/ru/easycode/zerotoheroandroidtdd/Task002Test.kt
+++ app/src/androidTest/java/ru/easycode/zerotoheroandroidtdd/Task002Test.kt
@@ -6,7 +6,6 @@ import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
