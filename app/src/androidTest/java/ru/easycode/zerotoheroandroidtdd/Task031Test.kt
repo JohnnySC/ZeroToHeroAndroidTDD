a3fc020b800de2ae9d2e862000d9cdbcd3f52b9b
hovhannes <johnnysc091@gmail.com>
2024-08-08 12:34:30 +0300

    task-031 compose click action

--- app/src/androidTest/java/ru/easycode/zerotoheroandroidtdd/Task031Test.kt
+++ app/src/androidTest/java/ru/easycode/zerotoheroandroidtdd/Task031Test.kt
@@ -1,20 +1,21 @@
 package ru.easycode.zerotoheroandroidtdd
 
+import androidx.compose.ui.test.assertHasClickAction
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class InitialUiTest {
+class Task031Test {
 
     @get:Rule
     val composeTestRule = createAndroidComposeRule<MainActivity>()
 
     @Test
-    fun test() {
-        //todo
+    fun test_click_me() {
+        composeTestRule.onNodeWithText("Click me!").assertHasClickAction()
     }
 }
