1f56d969a12b4df552bf1d56da382cbd35356f40
hovhannes <johnnysc091@gmail.com>
2024-09-12 19:44:33 +0900

    task-034 increment

---
+++ app/src/androidTest/java/ru/easycode/zerotoheroandroidtdd/Task034UiTest.kt
@@ -0,0 +1,35 @@
+package ru.easycode.zerotoheroandroidtdd
+
+import androidx.compose.ui.test.assertHasClickAction
+import androidx.compose.ui.test.assertHasNoClickAction
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class Task034UiTest {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+    @Test
+    fun increment(): Unit = with(composeTestRule) {
+        onNodeWithText("0").assertHasNoClickAction()
+        onNodeWithText("increment").assertHasClickAction()
+
+        onNodeWithText("increment").performClick()
+        onNodeWithText("0").assertDoesNotExist()
+        onNodeWithText("1").assertHasNoClickAction()
+
+        onNodeWithText("increment").performClick()
+        onNodeWithText("1").assertDoesNotExist()
+        onNodeWithText("2").assertHasNoClickAction()
+
+        activityRule.scenario.recreate()
+        onNodeWithText("2").assertHasNoClickAction()
+    }
+}
